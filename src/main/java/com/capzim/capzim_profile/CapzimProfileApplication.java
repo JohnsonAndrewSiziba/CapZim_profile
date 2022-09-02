a8d988015ea2144333eaf6e37991b67f451f39e3
Johnson Andrew Siziba <siziba.uz@outlook.com>
2022-09-02 08:14:04 +0200

    Initial Commit

---
+++ src/main/java/com/capzim/capzim_profile/CapzimProfileApplication.java
@@ -0,0 +1,13 @@
+package com.capzim.capzim_profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapzimProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapzimProfileApplication.class, args);
+	}
+
+}
